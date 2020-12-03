609ec9329939f413f296b31ba718ef61098c9c1b
rianne <2025798@talnet.nl>
2020-12-03 11:20:37 +0100

    git commit helper.php

---
+++ helper.php
@@ -0,0 +1,29 @@
+<?php 
+
+class Helper{
+
+    public function field_validation($fields){
+        // check if provided arg is an array. No need to try to loop non iterable arg.
+        if(is_array($fields)){
+            $error = false;
+
+            foreach($fields as $fieldname){
+                if(!isset($_POST[$fieldname]) || empty($fieldname)){
+                    $error = true;
+                    echo "The following field requires a value: $fieldname <br>";
+                }
+            }
+
+            if(!$error){
+                return true;
+            }
+
+            return false;
+        }else{
+            echo "Fields not iterable";
+        }
+    }
+
+}
+
+?>
